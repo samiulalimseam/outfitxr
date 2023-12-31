352032df87a9ada3dd90a670b045b0b2de4f1aaf
Samiul Alim <samiulalimseam@gmail.com>
2023-12-31 11:58:23 +0600

    added sso

--- web/frontend/components/popup/SsoPopup.jsx
+++ web/frontend/components/popup/SsoPopup.jsx
@@ -1,61 +1,85 @@
-import React, { useState } from "react";
-import { Button } from "@chakra-ui/react";
+import { useState, useRef, useEffect } from "react";
+import {
+  useDisclosure,
+  Modal,
+  Button,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+} from "@chakra-ui/react";
 
-const SsoPopup = ({ shopDataLoading, shop }) => {
+const SsoPopup = ({ shopDataLoading, update, setUpdate, shop }) => {
   const [isBtnLoading, setBtnLoading] = useState(false);
-  const [accessToken, setAccessToken] = useState("-");
-  console.log(accessToken)
+  const [accessToken, setAccessToken] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = useRef(null);
 
-  const openSsoPopup = () => {
-    // Your SSO login URL
-    const ssoLoginUrl = `https://www.outfitxr.io/sso/login?origin=${shop}`;
+  const ssoLoginUrl = `https://www.outfitxr.io/sso?origin=${shop}`;
 
-    const popup = window.open(
-      ssoLoginUrl,
-      "Login to OutfitterXR",
-      "width=600,height=600"
-    );
+  useEffect(() => {
+    // Function to handle post messages received from the iframe
+    const handleMessage = (event) => {
+      // Check if the message origin is trusted (you can adjust this according to your requirements)
+      if (event.origin === "https://www.outfitxr.io") {
+        // Access the data sent from the iframe
+        const messageData = event.data;
+        setAccessToken(JSON.stringify(messageData));
+        setAccessToken(JSON.stringify(messageData));
+        localStorage.setItem("outfitXrAccessToken", messageData);
+        onClose();
+        setUpdate(!update);
 
-    if (window.focus) {
-      popup.focus();
-    }
-    const checkPopupClosed = setInterval(() => {
-      try {
-        if (!popup || popup.closed || popup.closed === undefined) {
-          clearInterval(checkPopupClosed);
-          // Perform actions after the popup is closed
-        } else if (popup.location.href.includes("YOUR_SUCCESS_REDIRECT_URL")) {
-          clearInterval(checkPopupClosed);
-          // Successful login, retrieve access token from the popup
-          const urlParams = new URLSearchParams(popup.location.search);
-          const token = urlParams.get("access_token");
-          if (token) {
-            setAccessToken(token);
-            // Optionally, perform further actions with the token
-          }
-          popup.close();
-        }
-      } catch (error) {
-        console.error("Cross-origin access error:", error);
+        // Handle the received message data as needed
+        console.log("Received post message:", messageData);
       }
-    }, 1000);
-  };
+    };
+
+    // Add event listener for receiving post messages
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      // Clean up by removing the event listener when component unmounts
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
 
   return (
     <div>
       <Button
-        isLoading={shopDataLoading || isBtnLoading}
-        onClick={() => {
-          setBtnLoading(true);
-          openSsoPopup();
-          setTimeout(() => {
-            setBtnLoading(false);
-          }, 2000);
-        }}
         size={"sm"}
+        fontWeight={400}
+        bg={"green.500"}
+        color={"white"}
+        isDisabled={localStorage?.getItem("outfitXrAccessToken")?.length}
+        onClick={()=> {
+          setBtnLoading(true)
+          setTimeout(() => {
+            setBtnLoading(false)
+          }, 2000);
+          onOpen()
+        }}
       >
-        Connect
+        {localStorage?.getItem("outfitXrAccessToken")?.length
+          ? "Connected"
+          : "Connect"}{" "}
       </Button>
+      <Modal isOpen={isOpen} onClose={onClose} finalFocusRef={finalRef}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>SSO Login</ModalHeader>
+          <ModalBody>
+            {/* Create an iframe inside the modal */}
+            <iframe
+              title="SSO Login"
+              src={ssoLoginUrl}
+              width="100%"
+              height="400px"
+              frameBorder="0"
+            ></iframe>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
--- web/frontend/pages/Settings/index.jsx
+++ web/frontend/pages/Settings/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 import {
   Stack,
@@ -9,18 +9,28 @@ import {
   VStack,
   Text,
 } from "@chakra-ui/react";
-import { BsCoin, BsInfoCircle } from "react-icons/bs";
+import { BsCoin, BsInfoCircle , BsFillRecordFill } from "react-icons/bs";
 import SsoPopup from "../../components/popup/SsoPopup";
 import { useAppQuery } from "../../hooks/useAppQuery";
 
 export default function index() {
-const {data: shopData, isLoading : shopDataLoading } =     useAppQuery({url: '/api/shop',});
-
-
+  const [accessToken, setAccessToken] = useState('');
+  const [update, setUpdate] = useState(true);
+  const { data: shopData, isLoading: shopDataLoading } = useAppQuery({
+    url: "/api/shop",
+  });
+ 
+useEffect(()=> {
+const token = localStorage.getItem('outfitXrAccessToken')
+if(token?.length) {
+  setAccessToken(JSON.parse(token))
+}
+}, [update])
+console.log(accessToken)
 
   return (
     <Box p={10} my={"20"}>
-      <Flex direction={'column'} gap={10}>
+      <Flex direction={"column"} gap={10}>
         <HStack alignItems={"start"} gap={10}>
           <Box mt={2} w={"30%"}>
             <Text fontWeight={"bold"}>OutfitXR Content Management System</Text>
@@ -41,10 +51,20 @@ const {data: shopData, isLoading : shopDataLoading } =     useAppQuery({url: '/a
                   <Text fontWeight={"bold"} color={"black"}>
                     OutfitXR Content Management System
                   </Text>
-                  <Text color={"gray"}>No account connected</Text>
+                  <Text color={"gray"}>
+                    {accessToken ? accessToken?.origin
+                      : "No account connected"}
+                  </Text>
                 </Box>
                 {/* sso btn  */}
-                <SsoPopup shop={shopData?.shop} shopDataLoading={shopDataLoading} />
+                <SsoPopup
+                  
+                  accessToken={accessToken}
+                  update={update}
+                  setUpdate={setUpdate}
+                  shop={shopData?.shop}
+                  shopDataLoading={shopDataLoading}
+                />
               </Flex>
               <HStack borderRadius={"md"} bg={"blue.50"} p={4}>
                 <Text fontSize={"sm"} fontWeight={600} color={"blue.600"}>
@@ -86,16 +106,59 @@ const {data: shopData, isLoading : shopDataLoading } =     useAppQuery({url: '/a
                 </Box>
               </Flex>
               <Divider />
-              <Flex direction={'column'} borderRadius={"md"} bg={"blue.50"} p={4}>
-                <Text fontWeight={'black'}>
-                    Auto-renewal is disabled
-                </Text>
+              <Flex
+                direction={"column"}
+                borderRadius={"md"}
+                bg={"blue.50"}
+                p={4}
+              >
+                <Text fontWeight={"black"}>Auto-renewal is disabled</Text>
                 <Text fontSize={"sm"} fontWeight={600} color={"blue.600"}>
                   <BsInfoCircle
                     style={{ margin: "0 10px 0 0 ", display: "inline-block" }}
                   />
-                  Enable auto-renewal to automatically receive new credits every month .
+                  Enable auto-renewal to automatically receive new credits every
+                  month .
                 </Text>
+              </Flex>
+            </Flex>
+          </Box>
+        </HStack>
+        <HStack className="auto-renew-credits" alignItems={"start"} gap={10}>
+          <Box mt={2} w={"30%"}>
+            <Text fontWeight={"bold"}>Mode</Text>
+            <Text fontSize={"sm"} color={"gray"}>
+                      Select a mode to use OutfitXR
+              </Text>
+          </Box>
+          <Box w={"100%"}>
+            <Flex
+              gap={6}
+              direction={"column"}
+              borderRadius={"md"}
+              bg={"white"}
+              p={4}
+            >
+              <Flex gap={6}>
+                <Box w={"full"}>
+                  <Text fontWeight={"bold"} color={"black"}>
+                    Current Mode
+                  </Text>
+                  <Text color={"gray"}>
+                    {" "}
+                    <BsFillRecordFill style={{ display: "inline-block" }} /> Test{" "}
+                  </Text>
+                </Box>
+              </Flex>
+              <Divider />
+              <Flex
+                direction={"column"}
+                borderRadius={"md"}
+                bg={"blue.50"}
+                p={4}
+              >
+                <Text fontWeight={"black"}>Select the plan</Text>
+                
               </Flex>
             </Flex>
           </Box>
