2ca569614a12fabec4097e822c59041edf4a032f
Samiul Alim <samiulalimseam@gmail.com>
2024-01-01 12:24:12 +0600

    added new api key

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <PolarisProvider>
       <BrowserRouter>
-        <AppBridgeProvider>
+        <AppBridgeProvider  >
        
           <QueryProvider>
           <NavigationMenu
